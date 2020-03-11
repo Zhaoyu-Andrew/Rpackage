e5c11c9c00f03b15c37f219258f9e24110181952
Andrew Liu <zliu2@uw.edu>
2020-03-11 13:32:15 -0700

    add data

---
+++ R/my_gapminder.R
@@ -0,0 +1,17 @@
+#' The famous \code{\link[gapminder]{gapminder}} dataset taken from the
+#' \code{gapminder} package to practice adding datasets to our R package.
+#'
+#' @format a data frame with 1704 observations and 6 variables:
+#' \describe{
+#' \item{country}{factor with 142 levels}
+#' \item{continent}{factor with 5 levels}
+#' \item{year}{ranges from 1952 to 2007 in increments of 5 years}
+#' \item{lifeExp}{life expectancy at birth, in years}
+#' \item{pop}{population}
+#' \item{gdpPercap}{GDP per capita (US$, inflation-adjusted)}
+#' }
+#'
+#' @source
+#' http://www.gapminder.org/data/
+#'
+"my_gapminder"
