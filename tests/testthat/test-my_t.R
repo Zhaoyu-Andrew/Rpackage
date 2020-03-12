b12bbd2b3c61ac70ff6ecaf59d94705da2fdaa3d
Andrew Liu <zliu2@uw.edu>
2020-03-11 17:48:59 -0700

    add import

--- tests/testthat/test-my_t.R
+++ tests/testthat/test-my_t.R
@@ -1,14 +1,13 @@
-library(stringr)
-sample <- c(1,2,3,4,5)
+
 
 test_that("my_t.test less properly", {
-  expect_is(my_t.test(sample, "less", 1), "list")
+  expect_is(my_t.test(my_gapminder[[4]], "less", 1), "list")
 })
 
 test_that("my_t.test greater properly", {
-  expect_is(my_t.test(sample, "greater", 1), "list")
+  expect_is(my_t.test(my_gapminder[[4]], "greater", 1), "list")
 })
 
 test_that("my_t.test two.sided properly", {
-  expect_is(my_t.test(sample, "two.sided", 1), "list")
+  expect_is(my_t.test(my_gapminder[[4]], "two.sided", 1), "list")
 })
