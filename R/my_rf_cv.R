da8cf53569bb42edef3d48fd5b8afbfb9739c57a
Andrew Liu <zliu2@uw.edu>
2020-03-11 23:03:48 -0700

    change my_rf

--- R/my_rf_cv.R
+++ R/my_rf_cv.R
@@ -11,8 +11,10 @@
 #'
 #' @export
 my_rf_cv <- function(k) {
+  # get the total number of the dataset
   n <- nrow(dataset)
   inds <- sample(rep(1:k, length = n))
+  # randomly assigns observations to folds 1,…,k
   dataset[, "split"] <- inds
   pred_mat2 <- matrix(NA, n, 1)
   for(i in 1:k) {
@@ -20,10 +22,12 @@ my_rf_cv <- function(k) {
       filter(split != i)
     data_test <- dataset %>%
       filter(split == i)
+    # create the random forest model
     MODEL <- randomForest(lifeExp ~ gdpPercap, data = data_train, ntree = 100)
 
     pred_mat2[inds == i, 1] = predict(MODEL, data_test[, -1])
   }
+  # calculate the MSE across all k value
   MSE <- colMeans((pred_mat2 - dataset$lifeExp)^2)
   return(MSE)
 }
