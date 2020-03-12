ee94e1a51e6f2cb1f0977460671b2ad5db881184
Andrew Liu <zliu2@uw.edu>
2020-03-12 16:42:51 -0700

    make change

--- R/my_lm.R
+++ R/my_lm.R
@@ -11,7 +11,7 @@
 #' my_lm(mpg ~ hp + wt, data = mtcars)
 #'
 #' @export
-#' @import magrittr
+#' @import magrittr stats dplyr::filter
 my_lm <- function(formula, data) {
   matrix_x <- model.matrix(formula, data)
   matrix_y <- model.response(model.frame(formula, data))
--- R/my_rf_cv.R
+++ R/my_rf_cv.R
@@ -10,7 +10,7 @@
 #' my_rf_cv(5)
 #'
 #' @export
-#' @import class magrittr randomForest dplyr
+#' @import class magrittr randomForest stats dplyr::filter
 my_rf_cv <- function(k) {
   # get the total number of the dataset
   n <- nrow(my_gapminder)
