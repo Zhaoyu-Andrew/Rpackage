12ead08d387c52a0526f4cb8523a1efc8c9f36c7
Andrew Liu <zliu2@uw.edu>
2020-03-14 16:45:19 -0700

    add one more test for t test

--- tests/testthat/test-my_t.R
+++ tests/testthat/test-my_t.R
@@ -1,13 +1,17 @@
 test_that("my_t.test less properly", {
-  expect_is(my_t.test(my_gapminder[[4]], "less", 1), "list")
+  expect_is(my_t.test(my_gapminder[[4]], "less", 60), "list")
 })
 
 test_that("my_t.test greater properly", {
-  expect_is(my_t.test(my_gapminder[[4]], "greater", 1), "list")
+  expect_is(my_t.test(my_gapminder[[4]], "greater", 60), "list")
 })
 
 test_that("my_t.test two.sided properly", {
-  expect_is(my_t.test(my_gapminder[[4]], "two.sided", 1), "list")
+  expect_is(my_t.test(my_gapminder[[4]], "two.sided", 60), "list")
+})
+
+test_that("my_t.test two.sided properly", {
+  expect_is(my_t.test(my_gapminder[[6]], "two.sided", 60), "list")
 })
 
 test_that("String input throws error", {
