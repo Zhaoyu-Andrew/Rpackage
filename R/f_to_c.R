b2d0f1a9e8fa419470feb53a98a7afca1cbb404e
Andrew Liu <zliu2@uw.edu>
2020-03-14 20:13:44 -0700

    add work from lab session

---
+++ R/f_to_c.R
@@ -0,0 +1,17 @@
+#' Fahrenheit converter
+#'
+#' This function converts temperature from Fahrenheit to Celsius.
+#'
+#' @param temp_F Numeric indicating temperature in Fahrenheit.
+#' @keywords temperature
+#' @return Numeric indicating temperature \code{temp_F} converted to Celsius.
+#'
+#' @example
+#' f_to_c(32)
+#' f_to_c(212)
+#'
+#' @export
+f_to_c <- function(temp_F) {
+  temp_C <- (temp_F - 32) * 5 / 9
+  return(temp_C)
+}
